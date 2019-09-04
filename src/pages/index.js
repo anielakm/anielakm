c92423abe06c80f1230a5d042ec9865b9ec55568
Aniela <kontakt@anielakm.pl>
2019-09-04 12:22:32 +0200

    Add based components and files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,29 @@
-import React from "react"
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import Header from "../components/Header";
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
+const Global = createGlobalStyle`
+body{
+  padding:0;
+  margin:0;
+  font-family: 'Montserrat';
+}
+*, *::before, *::after{
+  box-sizing:border-box;
+}
+`;
 
 const IndexPage = () => (
-  <>index</>
-)
+  <>
+    <Global />
+    <Header />
+    <About />
+    <Projects />
+    <Contact />
+  </>
+);
 
-export default IndexPage
+export default IndexPage;
---
+++ src/utilis/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    basedBeige: "#dbcabb",
+    darkerBeige: "#998f83",
+    pistachio: "#dbd3bb",
+    warmBeige: "#dbc5bb",
+    darkColdBeige: "#998a83"
+  }
+};
