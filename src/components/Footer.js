4cedc160e218d85ec44d883347e716d1c533e219
Aniela <kontakt@anielakm.pl>
2019-09-05 11:51:26 +0200

    Added footer styling for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import { theme } from "../utilis/theme";
 
+const FooterContainer = styled.div`
+  background-color: ${theme.colors.darkBeige};
+  font-family: ${theme.fonts.lato};
+  color: rgba(255, 255, 255, 0.8);
+  letter-spacing: 1px;
+  font-size: 10px;
+  text-align: center;
+  padding: 2%;
+`;
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <>
+      <FooterContainer>
+        Template created by Aniela Katana-Matłok. All rights reserved.
+      </FooterContainer>
+    </>
+  );
 };
 
 export default Footer;
