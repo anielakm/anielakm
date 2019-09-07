1065e1712bda51b331bb3a92024bdfd458044b78
Aniela <kontakt@anielakm.pl>
2019-09-07 13:53:28 +0200

    fixed form to integrate it with netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -166,7 +166,7 @@ const Contact = () => {
           action="/success"
           data-netlify="true"
           data-netlify-honeypot="bot-field">
-          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
           <Inputs>
             <input type="text" placeholder="name" name="name" />
             <br />
