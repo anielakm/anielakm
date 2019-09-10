e896541f95f6c2b1fbdfd6fc765250a13baf7a4f
Aniela <kontakt@anielakm.pl>
2019-09-10 11:36:54 +0200

    integrate form with netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,6 +4,16 @@ import { theme } from "../utilis/theme";
 import H3 from "../components/H3/H3";
 import Recaptcha from "react-recaptcha";
 
+const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
+if (typeof RECAPTCHA_KEY === 'undefined') {
+    throw new Error(`
+  Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
+  You probably forget to set it in your Netlify build environment variables. 
+  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
+  Note this demo is specifically for Recaptcha v2
+  `)
+}
+
 const ContactContainer = styled.div`
   background-color: ${theme.colors.darkBeige};
   padding: 3% 5%;
@@ -28,15 +38,14 @@ const Inputs = styled.div`
   margin: 1% 0;
   border-radius: 5px;
 
-  text-align: center;
+  label{
+      font-size:14px;
+        padding:8% 1%;
+      color: gray;
 
-  div {
-    margin: auto;
-    padding: auto;
-  }
 
   input {
-    margin: 0 0 3% 0;
+    margin: 2% 0 3% 0;
     width: 304px;
 
     color: gray;
@@ -52,12 +61,20 @@ const Inputs = styled.div`
     }
 
     @media (min-width: 768px) {
-      margin: 0 0 4% 0;
+      margin: 3% 0 4% 0;
       font-size: 14px;
       padding: 2%;
       line-height: 22px;
+      
     }
   }
+  }
+
+  div {
+    margin: auto;
+    padding: auto;
+  }
+
 
   @media (min-width: 1024px) {
     width: auto;
@@ -88,6 +105,7 @@ const Message = styled.div`
     font-size: 12px;
     height: 150px;
     border: none;
+    min-width:304px;
     box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
     &:active,
     &:focus {
@@ -187,11 +205,12 @@ export default function Contact() {
         <ContactContainer>
             <H3 light>Kontakt</H3>
             <Form
+
                 name="contact"
                 method="post"
                 action="/thanks/"
                 data-netlify="true"
-                data-netlify-honeypot="bot-field"
+                data-netlify-recaptcha="true"
                 onSubmit={handleSubmit}
             >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
@@ -204,24 +223,22 @@ export default function Contact() {
                 </p>
                 <Inputs>
 
+                    <label > Imię: <br />
+                        <input type="text" name="name" onChange={handleChange} placeholder="imię" />
+                    </label>
                     <br />
-                    <input type="text" name="name" onChange={handleChange} placeholder="imię" />
 
+                    <label htmlFor="">E-mail: <br />
+                        <input type="email" name="email" onChange={handleChange} placeholder="email" />
+                    </label>
 
-
-                    <br />
-                    <input type="email" name="email" onChange={handleChange} placeholder="email" />
-
+                    <br /> <label>  <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} /></label>
 
                 </Inputs>
                 <Message>
 
-
-
                     <br />
-                    <textarea name="message" onChange={handleChange} placeholder="Treść wiadomości ..." />
-
-
+                    <textarea name="message" onChange={handleChange} placeholder="Treść wiadomości ..." /> <br />
                     <button type="submit">Wyślij wiadomość</button>
 
                 </Message>
