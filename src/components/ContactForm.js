cbc54aa43c27f4152e2ff319dc205e745c94673d
Aniela <kontakt@anielakm.pl>
2019-09-10 12:41:33 +0200

    fixing contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,147 @@
 import React from 'react'
-import { navigate } from 'gatsby'
 import Recaptcha from 'react-google-recaptcha'
+import styled from "styled-components";
+import { theme } from "../utilis/theme";
+import H3 from "../components/H3/H3";
+
+const ContactContainer = styled.div`
+  background-color: ${theme.colors.darkBeige};
+  padding: 3% 5%;
+  font-family: ${theme.fonts.lato};
+  @media (min-width: 1024px) {
+    padding: 1% 2% 2% 2%;
+  }
+`;
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+`;
+
+const Inputs = styled.div`
+  background-color: ${theme.colors.baseBeige};
+  opacity: 0.9;
+  padding: 5%;
+  margin: 1% 0;
+  border-radius: 5px;
+  label{
+      font-size:14px;
+        padding:8% 1%;
+      color: gray;
+  input {
+    margin: 2% 0 3% 0;
+    width: 304px;
+    color: gray;
+    padding: 3%;
+    font-size: 12px;
+    border: none;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
+    &:active,
+    &:focus {
+      outline: 1px solid ${theme.colors.darkBeige};
+      border: 1px solid ${theme.colors.darkBeige};
+    }
+    @media (min-width: 768px) {
+      margin: 3% 0 4% 0;
+      font-size: 14px;
+      padding: 2%;
+      line-height: 22px;
+      
+    }
+  }
+  }
+  div {
+    margin: auto;
+    padding: auto;
+  }
+  @media (min-width: 1024px) {
+    width: auto;
+    justify-content: center;
+    align-items: center;
+    padding: 2%;
+    input {
+      width: 304px;
+    }
+  }
+`;
+
+const Message = styled.div`
+  background-color: ${theme.colors.baseBeige};
+  opacity: 0.9;
+  padding: 5%;
+  margin: 1% 0;
+  text-align: center;
+  border-radius: 5px;
+  @media (min-width: 768px) {
+  }
+  textarea {
+    width: 100%;
+    color: gray;
+    padding: 4%;
+    font-size: 12px;
+    height: 150px;
+    border: none;
+    min-width:304px;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
+    &:active,
+    &:focus {
+      outline: 1px solid ${theme.colors.darkBeige};
+      border: 1px solid ${theme.colors.darkBeige};
+    }
+    @media (min-width: 768px) {
+      padding: 2%;
+      line-height: 18px;
+      width: 304px;
+    }
+    @media (min-width: 1024px) {
+      padding: 2%;
+      line-height: 18px;
+      width: 100%;
+    }
+  }
+  button {
+    background-color: ${theme.colors.darkBeige};
+    border: 2px solid ${theme.colors.darkBeige};
+    border-radius: 30px;
+    padding: 4% 9%;
+    color: white;
+    letter-spacing: 1px;
+    font-weight: 400;
+    font-size: 10px;
+    text-transform: uppercase;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.15);
+    margin-top: 4%;
+    outline: none;
+    transition: background-color 1s, color 0.3s, font-weight 0.3s, box-shadow 1s;
+    :hover {
+      background-color: rgba(255, 255, 255, 0.3);
+      color: ${theme.colors.darkBeige};
+      cursor: pointer;
+      font-weight: 500;
+      box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.25);
+    }
+    @media (min-width: 768px) {
+      font-size: 10px;
+      letter-spacing: 2px;
+      margin-top: 4%;
+      padding: 2% 4%;
+    }
+  }
+  @media (min-width: 1024px) {
+    padding: 2%;
+    margin: 1%;
+    width: 50%;
+    button {
+      font-size: 11px;
+    }
+  }
+  @media (min-width: 1224px) {
+    width: 600px;
+  }
+`;
+
 
 const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
 if (typeof RECAPTCHA_KEY === 'undefined') {
@@ -44,9 +185,9 @@ export default function ContactForm() {
   }
 
   return (
-    <>
-      <h1>reCAPTCHA 2</h1>
-      <form
+    <ContactContainer>
+      <H3 light>Kontakt</H3>
+      <Form
         name="contact-recaptcha"
         method="post"
         action="/thanks/"
@@ -57,32 +198,31 @@ export default function ContactForm() {
         <noscript>
           <p>This form won’t work with Javascript disabled</p>
         </noscript>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
+
+        <Inputs>
+
+          <label > Imię: <br />
+            <input type="text" name="name" onChange={handleChange} placeholder="imię" />
           </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
+          <br />
+
+          <label htmlFor="">E-mail: <br />
+            <input type="email" name="email" onChange={handleChange} placeholder="email" />
           </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </>
+
+          <br /> <label>  <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} /></label>
+
+        </Inputs>
+
+
+        <Message>
+
+          <br />
+          <textarea name="message" onChange={handleChange} placeholder="Treść wiadomości ..." /> <br />
+          <button type="submit">Wyślij wiadomość</button>
+
+        </Message>
+      </Form>
+    </ContactContainer>
   )
 }
